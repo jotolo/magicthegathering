5804e85d5db0ed5bf3c45b6696796fd64922a3dc
Jorge T <jotolo7@gmail.com>
2018-03-22 16:24:34 -0400

    Adding MagicTheGatheringAPI to consume API. Adding dependencies in Gemfile and specifying ruby version

---
+++ magic_the_gathering_api.rb
@@ -0,0 +1,35 @@
+require 'httparty'
+class MagicTheGatheringAPI
+  include HTTParty
+  base_uri 'api.magicthegathering.io'
+  attr_accessor :options
+
+  def initialize(service)
+    @options = { query: { site: service, page: 1 } }
+  end
+
+  # Get cards from specific page
+  def cards(page)
+    self.options[:query][:page] = page || 1
+    http_response = self.class.get("/v1/cards", @options)
+    json_response = JSON.parse(http_response.body)
+    cards = []
+    json_response["cards"].each do |json_card|
+      cards << Card.new(json_card)
+    end
+    cards
+  end
+
+  # Get all cards
+  def all_cards
+    cards_collection = []
+    page = 1
+    loop do
+      cards_page = self.cards(page)
+      break if cards_page.empty? || page > 10 #TODO: remove this
+      cards_collection += cards_page
+      page += 1
+    end
+    cards_collection
+  end
+end
